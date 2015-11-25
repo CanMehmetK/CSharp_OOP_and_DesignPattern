58491c2d4c8cc8113e123faaf3f828cc86f654ca
Mehmet Kanpınar <kanpinar@hotmail.com>
2015-11-25 15:10:00 +0200

    Readme creational Patterns Folder

--- DesignPatterns/CreationalPatterns/Notlar.cs
+++
@@ -1,4 +0,0 @@
-﻿// TODO@All: Lütfen tarih ve isim ekleyerek yazın. 
-
-// http://www.uml-diagrams.org/class-diagrams-overview.html
-// http://www.uml-diagrams.org/design-pattern-abstract-factory-uml-class-diagram-example.html
